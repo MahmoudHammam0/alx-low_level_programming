5725d4a2ec4f3e3fa549e840c35100c5397dedde
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-15 00:28:44 -0700

    Sign

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -81,3 +81,25 @@ int _isalpha(int c)
 	}
 	return (c);
 }
+/**
+ * print_sign:check the sign of the number
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (n);
+}
