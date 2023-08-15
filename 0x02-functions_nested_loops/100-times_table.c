947f477284d095201f088d4529c1e1b7cb6cdf2f
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-15 13:08:12 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,76 +1,49 @@
 #include "main.h"
 /**
- * print_times_table - print times table of any given number
- * @z: input number to function printz
- * Description: print times table of number n
- * Return: nothing (Void)
+ * print_times_table - prints the times table for a given number
+ * @n: the number to print its times table
+ * Return: Nothing (void)
  */
-int printz(int z);
 void print_times_table(int n)
 {
-	int a = 0;
+	int a;
+	int b;
+	int x;
 
-	if (n > 15 || n < 0)
+	if (!(n > 15 || n < 0))
 	{
-		return;
-	}
-	while (a <= n)
-	{
-		int b = 0;
-
-		while (b <= n)
+		for (a = 0; a <= n; a++)
 		{
-			int x = (a * b);
-
-			if (b != 0)
+			for (b = 0; b <= n; b++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				x = (a * b);
+				if (b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (x < 10 && b != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((x % 10) + '0');
+				}
+				else if (x >= 10 && x < 100)
+				{
+					_putchar(' ');
+					_putchar((x / 10) + '0');
+					_putchar((x % 10) + '0');
+				}
+				else if (x >= 100 && b != 0)
+				{
+					_putchar((x / 100) + '0');
+					_putchar((x / 10) % 10 + '0');
+					_putchar((x % 10) + '0');
+				}
+				else
+					_putchar((x % 10) + '0');
 			}
-			if (x <= 10 && b != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((x % 10) + '0');
-			}
-			else if (x >= 10 && x < 100)
-			{
-				_putchar(' ');
-				_putchar((x / 10) + '0');
-				_putchar((x % 10) + '0');
-			}
-			else if (x >= 100 && b != 0)
-			{
-				_putchar((x / 100) + '0');
-				_putchar((x % 10) % 10 + '0');
-				_putchar((x % 10) + '0');
-			}
-			else if (x < 10 && b != 0)
-			{
-				_putchar(' ');
-				_putchar((x % 10) + '0');
-			}
-			else
-			{
-				_putchar((x % 10) + '0');
-			}
-			b++;
+			_putchar('\n');
 		}
-		_putchar('\n');
-		a++;
 	}
-}
-/**
- * printz - print type int from _putchar
- * @z: input number to function printz
- * Return: Always 0(Success)
- */
-int printz(int z)
-{
-	if (z != 0)
-	{
-		printz(z / 10);
-		_putchar((z % 10) + '0');
-	}
-	return (0);
 }
