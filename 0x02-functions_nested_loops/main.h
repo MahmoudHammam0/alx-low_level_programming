7070685f6248cd98ef3c2c1d65f18c99ed7c7fd8
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-14 23:52:22 -0700

    islower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -46,3 +46,18 @@ void print_alphabet_x10(void)
 		x++;
 	}
 }
+/**
+ * _islower: check if the character is uppercase or lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (0);
+	}
+	return (c);
+}
