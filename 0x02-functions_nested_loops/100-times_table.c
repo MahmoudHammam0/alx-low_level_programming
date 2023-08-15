7b1b077d182ffbe049e8ba8fdf7093f18f4d6277
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-15 12:57:53 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * print_times_table - print times table of any number
+ * print_times_table - print times table of any given number
  * @z: input number to function printz
- * Return: nothing
+ * Description: print times table of number n
+ * Return: nothing (Void)
  */
 int printz(int z);
 void print_times_table(int n)
 {
-	int a = 0;
-
 	if (n > 15 || n < 0)
 	{
 		return;
 	}
+	int a = 0;
+
 	while (a <= n)
 	{
 		int b = 0;
@@ -25,15 +26,23 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
+			}
+			if (x <= 10 && b != 0)
+			{
 				_putchar(' ');
+				_putchar(' ');
+				_putchar((x % 10) + '0');
 			}
-			if (x >= 100)
+			else if (x >= 10 && x < 100)
 			{
-				printz(x);
-			}
-			else if (x >= 10)
-			{
+				_putchar(' ');
 				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+			}
+			else if (x >= 100 && b != 0)
+			{
+				_putchar((x / 100) + '0');
+				_putchar((x % 10) % 10 + '0');
 				_putchar((x % 10) + '0');
 			}
 			else if (x < 10 && b != 0)
@@ -52,15 +61,15 @@ void print_times_table(int n)
 	}
 }
 /**
- * printz - print int from _putchar
+ * printz - print type int from _putchar
  * @z: input number to function printz
- * Return: int from putchar
+ * Return: Always 0(Success)
  */
 int printz(int z)
 {
 	if (z != 0)
 	{
-		printz(z / 10);
+		_printz(z / 10);
 		_putchar((z % 10) + '0');
 	}
 	return (0);
