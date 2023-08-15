002847e42a3688d8f219981c3e39d76a342f3a1d
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-14 23:25:06 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #include <unistd.h>
-
+#include <stdio.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -10,4 +10,39 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+/**
+ * print_alphabet - print alphabet in lowercase followed by new line
+ * Return: Always 0(Success)
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
+/**
+ * print_alphabet_x10 - print alphabet in lowercase ten times
+ */
+void print_alphabet_x10(void)
+{
+	int x = 1;
+
+	while (x <= 10)
+	{
+		char a = 'a';
+
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		x++;
+	}
 }
