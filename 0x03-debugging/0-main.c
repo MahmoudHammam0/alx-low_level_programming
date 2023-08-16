e319e2349b90fb9b8eeb7fc918f86bedf870491e
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-16 13:55:01 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: test positive or negative function
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
