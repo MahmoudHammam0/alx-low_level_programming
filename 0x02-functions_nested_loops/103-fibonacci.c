2b8eeb0d0bc5356e4bb67269061fdd64a4848a36
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-15 19:11:36 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Description: print sum of all even values in fibonacci sequence under 4m
+  * Return: Always 0(Success)
+  */
+int main(void)
+{
+	unsigned long count;
+	unsigned long a;
+	unsigned long b;
+	unsigned long c;
+	unsigned long result;
+
+	a = sums = 0;
+	b = 1;
+	for (count = 0; count < 50; count++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c % 2 == 0 && k < 4000000)
+		{
+			result = (result + k);
+		}
+	}
+	printf("%lu\n", result);
+	return (0);
+}
