832bc0e662785e533f10a923b1b94748415c2228
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-16 16:29:11 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,6 +20,22 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
+	else if (a == b && a < c)
+	{
+		largest = a;
+	}
+	else if (a == b && c > a)
+	{
+		largest = c;
+	}
+	else if (c == b && c > a)
+	{
+		largest = c;
+	}
+	else if (c == b && a > c)
+	{
+		largest = a;
+	}
 	else
 	{
 		largest = c;
