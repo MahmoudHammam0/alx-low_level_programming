7d78bc74498076e77eb61979449d7bf1f8ee8163
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-17 11:02:32 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,12 +17,12 @@ int main(void)
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 			printf(" ");
 		}
 		else if (a % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 			printf(" ");
 		}
 		else
