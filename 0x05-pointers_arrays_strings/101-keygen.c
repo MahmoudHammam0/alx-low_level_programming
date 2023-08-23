38e8e746dff35321cacccc10b9876f9d42b8bc14
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-22 18:22:13 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,13 +14,13 @@ int main(void)
 
 	srand(time(0));
 
-	while (x <= 2645)
+	while (x <= 2646)
 	{
-		a = rand() % 125;
+		a = rand() % 128;
 		write(1, &a, 1);
 		x = x + a;
 	}
-	a = 2800 - x;
+	a = 2772 - x;
 	write(1, &a, 1);
 	return (0);
 }
