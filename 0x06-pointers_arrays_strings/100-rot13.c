099bc354b37926410c139850a266edb58e6ba707
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-24 02:56:59 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13.
+ * @s : pointer to first element of string
+ * Return: pointer to character rotated
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char x[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		     'Z'};
+	char y[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		     'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		     'M'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 53; j++)
+		{
+			if (x[j] == s[i])
+			{
+				s[i] = y[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
