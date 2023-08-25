7c7766cb7621c6084d20ca9bc3383dc957b5ef5e
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-24 18:36:17 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer to store the result
+ * @size_r: size of (r) buffer
+ * Return: pointer of r or 0
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int a;
+	int b;
+	int v;
+	int z;
+	int i;
+	int j;
+	
+	for (a = 0; n1[a]; a++)
+		;
+	for (b = 0; n2[b]; b++)
+		;
+	if (a > size_r || b > size_r)
+		return (0);
+	i = 0;
+	for (a = a - 1, b = b - 1, v = 0; v < size_r - 1; a--, b--, v++)
+	{
+		j = i;
+		if (a >= 0)
+			j = j + n1[a] - '0';
+		if (b >= 0)
+			j = j + n2[b] - '0';
+		if (a < 0 && b < 0 && j == 0)
+		{
+			break;
+		}
+		i = j / 10;
+		r[v] = j % 10 + '0';
+	}
+	r[v] = '\0';
+	if (a >= 0 || b >= 0 || i)
+		return (0);
+	for (v = v - 1, z = 0; z < v; v--, z++)
+	{
+		i = r[v];
+		r[v] = r[z];
+		r[z] = i;
+	}
+	return (r);
+}
