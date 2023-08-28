924edf607c65a4845eaa931bb7a7e013b15e177d
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-28 04:55:12 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,19 +10,15 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 	int j;
 
-	if (!needle)
-	{
-		return (haystack);
-	}
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[j] && haystack[i + 1] == needle[j + 1])
-			{
-				return (haystack + i);
-			}
+			if (haystack[i + j] != needle[j])
+				break;
 		}
+		if (!needle[j])
+			return (haystack[i]);
 	}
 	return (NULL);
 }
