1df2779852962b11637e6aa9fb66d3c52f5f044e
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-08-31 23:20:47 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point to program
+ * @argc: number of argument passed
+ * @argv: array of strings first program name then rest of arguments
+ * Return: Always 0(Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] >= '0' && *argv[i] <= '9')
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				break;
+			}
+		}
+		if (sum > 0 && i == argc)
+		{
+			printf("%d\n", sum);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	return (0);
+}
