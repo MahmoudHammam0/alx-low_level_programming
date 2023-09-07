4976239d8bf49ca2835cdfb04a1c09cc9317c995
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-09-07 03:44:14 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	p = malloc(nmemb * sizeof(size));
+	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
