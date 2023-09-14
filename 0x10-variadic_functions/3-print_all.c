2d374b4464b50a392c07426a52a0d845eebc89aa
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-09-14 09:12:58 -0700

    To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,81 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * print_int - print integer for print all function
+ * @args: arguments list
+ * Return: Nothing
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+/**
+ * print_char - print character for print all function
+ * @args: arguments list
+ * Return: Nothing
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+/**
+ * print_float - print integer for print all function
+ * @args: arguments list
+ * Return: Nothing
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+/**
+ * print_str - print string for print all function
+ * @args: arguments list
+ * Return: Nothing
+ */
+void print_str(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+	if (s == NULL)
+	{
+		s = "(nil)";
+	}
+	printf("%s", s);
+}
+/**
+ * print_all - prints anything
+ * @format: string contain each format character
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	fmt c[] = {'i', print_int}
+	{'c', print_char}
+	{'f', print_float}
+	{'s', print_str}
+	{'\0', NULL};
+	va_list args;
+	int i = 0, j = 0;
+	char *v = "";
+
+	va_start(args, format);
+	while (format[i] && format != '\0')
+	{
+		while (c[j].x != '\0')
+		{
+			if (format[i] == c[j].x)
+			{
+				printf("%s", v);
+				c[j].ptr(args);
+				v = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,4 +3,14 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+/**
+ * struct print - struct to be used in print all function
+ * @x: format character
+ * @ptr: function pointer to print corresponding function
+ */
+typedef struct print
+{
+	char x;
+	void (*ptr)();
+} fmt
 #endif
