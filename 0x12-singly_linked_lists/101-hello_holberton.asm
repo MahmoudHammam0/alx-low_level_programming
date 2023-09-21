68a0c48ea0e591897d6ab2b5be7b4fbaef1a6fa0
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-09-21 04:07:04 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,18 @@
-section .data
-    msg db "Hello, Holberton",10      ; 10 is the ASCII code for a new line (LF)
+global _start
 
 section .text
-    global _start
 
 _start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, 13
-    syscall
-    
-    mov rax, 60
-    mov rdi, 0
-    syscall
+  mov rax, 1        ; write(
+  mov rdi, 1        ;   STDOUT_FILENO,
+  mov rsi, msg      ;   "Hello, Holberton",
+  mov rdx, msglen   ;   sizeof("Hello, Holberton\n")
+  syscall           ; );
+
+  mov rax, 60       ; exit(
+  mov rdi, 0        ;   EXIT_SUCCESS
+  syscall           ; );
+
+section .rodata
+  msg: db "Hello, Holberton", 10
+  msglen: equ $ - msg
