6a1e389abf00697bbb9d7e1096f65d8bfe989a1b
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-09-26 16:11:27 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,53 @@
 #include "lists.h"
 /**
+ *find_loop - find a loop in listint_t linked list.
+ *@head: pointer to the first node of the list
+ *Return: address where loop start in the list
+ */
+listint_t *find_loop(listint_t *head)
+{
+	listint_t *start;
+	listint_t *ptr;
+
+	if (head == NULL)
+		return (NULL);
+	ptr = head->next;
+	while (ptr != NULL)
+	{
+		if (ptr == ptr->next)
+			return (ptr);
+		start = head;
+		while (start != ptr)
+		{
+			if (start == ptr->next)
+				return (ptr->next);
+			start = start->next;
+		}
+		ptr = ptr->next;
+	}
+	return (NULL);
+}
+/**
  *print_listint_safe - prints a listint_t linked list.
  *@head: pointer to the first node of the list
  *Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *ptr = head, *ptr2 = head;
-	int count = 0;
+	int n = 0, i;
+	listint_t *ptr;
 
-	if (head == NULL)
-		exit(98);
-	while (ptr && ptr2 && ptr2->next && head)
+	ptr = find_loop((listint_t *)head);
+	i = 1;
+	while (head != NULL && (head != ptr || i))
 	{
-		ptr = ptr->next;
-		ptr2 = ptr2->next->next;
-		if (ptr == ptr2)
-		{
-			printf("->[%p] %d\n", (void *)head, head->n);
-			exit(98);
-		}
 		printf("[%p] %d\n", (void *)head, head->n);
+		if (head == ptr)
+			i = 0;
 		head = head->next;
-		count++;
+		n++;
 	}
-	head = NULL;
-	return (count);
+	if (ptr != NULL)
+		printf("->[%p] %d\n", (void *)head, head->n);
+	return (n);
 }
