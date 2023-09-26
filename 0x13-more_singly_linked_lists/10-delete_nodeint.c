8cbf5c14c2aa79ee53601d0e27ab147ebc99ef09
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-09-25 23:24:56 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (head == NULL)
 		return (-1);
 	else if (*head == NULL)
-		return (-1);
+		return (1);
 	ptr = *head;
 	if (index == 0)
 	{
