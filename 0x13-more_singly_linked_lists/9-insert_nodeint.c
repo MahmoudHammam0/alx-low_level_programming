318c8eaac61738767d05c08fc7a6c902a2bfb7fe
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-09-25 22:30:44 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (*head);
 	}
 	idx--;
-	for (ptr = *head; i != idx; i++)
+	for (ptr = *head; ptr && (i != idx); i++)
 		ptr = ptr->next;
 	if (ptr == NULL)
 		return (NULL);
