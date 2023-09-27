799edc69775b43883e34f5bc2b753f385a6ab12a
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-09-26 19:45:10 -0700

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,7 +11,7 @@
 const listint_t **new(const listint_t **old, size_t size, const listint_t *new)
 {
 	const listint_t **ptr;
-	size_t x = 0;
+	size_t i;
 
 	ptr = malloc(size * sizeof(listint_t *));
 	if (ptr == NULL)
@@ -19,12 +19,9 @@ const listint_t **new(const listint_t **old, size_t size, const listint_t *new)
 		free(old);
 		exit(98);
 	}
-	while (x < (size - 1))
-	{
-		ptr[x] = old[x];
-		x++;
-	}
-	ptr[x] = new;
+	for (i = 0; i < size - 1; i++)
+		ptr[i] = old[i];
+	ptr[i] = new;
 	free(old);
 	return (ptr);
 }
@@ -37,24 +34,24 @@ const listint_t **new(const listint_t **old, size_t size, const listint_t *new)
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t x = 0, n = 0;
+	size_t i, n = 0;
 	const listint_t **ptr = NULL;
 
-	for ( ; head != NULL; head = head->next)
+	while (head != NULL)
 	{
-		while (x < n)
+		for (i = 0; i < n; i++)
 		{
-			if (head == ptr[x])
+			if (head == ptr[i])
 			{
 				printf("-> [%p] %d\n", (void *)head, head->n);
 				free(ptr);
 				return (n);
 			}
-			x++;
 		}
 		n++;
 		ptr = new(ptr, n, head);
 		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
 	free(ptr);
 	return (n);
