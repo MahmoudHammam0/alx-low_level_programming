a19d69ce9cd8910c312ae6d83179ad7fabc75479
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-09-28 02:58:56 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -24,20 +24,22 @@ int check(const char *s)
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i, c, num, x, sum = 0, p = 1;
+	int i, c, num = 0;
 
 	if (b == NULL)
 		return (0);
 	c = check(b);
 	if (c == 1)
 		return (0);
-	num = atoi(b);
-	for (i = 0; num != 0; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		x = num % 10;
-		num = num / 10;
-		sum = sum + x * p;
-		p *= 2;
+		if (b[i] == '1')
+		{
+			num = num << 1;
+			num = num | 1;
+		}
+		else if (b[i] == '0')
+			num = num << 1;
 	}
-	return (sum);
+	return (num);
 }
