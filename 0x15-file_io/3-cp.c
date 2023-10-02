0f2a46659753a6c1a5119244a01ce00cb2116d56
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-10-02 06:53:00 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,7 +42,7 @@ void files(char *s1, char *s2)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s1);
 		exit(98);
 	}
-	fdt = open(s2, O_WRONLY | O_TRUNC | O_CREAT,
+	fdt = open(s2, O_CREAT | O_WRONLY | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fdt < 0)
 	{
