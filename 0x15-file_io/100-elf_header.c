cde561c1ee840886aa065cc35620208e70ec3b14
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-10-02 11:33:15 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -46,24 +46,6 @@ void magic(unsigned char *ptr)
 }
 
 /**
- *class - get the class of an ELF header.
- *@ptr: A pointer to the ELF class array.
- *Return: Nothing
- */
-void class(unsigned char *ptr)
-{
-	printf("  Class:                             ");
-	if (ptr[EI_CLASS] == ELFCLASSNONE)
-		printf("none\n");
-	else if (ptr[EI_CLASS] == ELFCLASS32)
-		printf("ELF32\n");
-	else if (ptr[EI_CLASS] == ELFCLASS64)
-		printf("ELF64\n");
-	else
-		printf("<unknown: %x>\n", ptr[EI_CLASS]);
-}
-
-/**
  *data - gets data of an ELF header.
  *@ptr: A pointer to the ELF class array.
  *Return: Nothing
@@ -80,22 +62,6 @@ void data(unsigned char *ptr)
 	else
 		printf("<unknown: %x>\n", ptr[EI_CLASS]);
 }
-
-/**
- *version - gets the version of an ELF header.
- *@ptr: A pointer to ELF version array.
- *Return: Nothing
- */
-void version(unsigned char *ptr)
-{
-	printf("  Version:                           %d",
-			ptr[EI_VERSION]);
-	if (ptr[EI_VERSION] == EV_CURRENT)
-		printf(" (current)\n");
-	else
-		printf("\n");
-}
-
 /**
  *osabi - gets the OS/ABI of an ELF header.
  *@ptr: A pointer to ELF version array.
@@ -139,6 +105,60 @@ void abi(unsigned char *ptr)
 			ptr[EI_ABIVERSION]);
 }
 
+
+
+/**
+ *class - get the class of an ELF header.
+ *@ptr: A pointer to the ELF class array.
+ *Return: Nothing
+ */
+void class(unsigned char *ptr)
+{
+	printf("  Class:                             ");
+	if (ptr[EI_CLASS] == ELFCLASSNONE)
+		printf("none\n");
+	else if (ptr[EI_CLASS] == ELFCLASS32)
+		printf("ELF32\n");
+	else if (ptr[EI_CLASS] == ELFCLASS64)
+		printf("ELF64\n");
+	else
+		printf("<unknown: %x>\n", ptr[EI_CLASS]);
+}
+/**
+ *entry - Prints the entry point of an ELF header.
+ *@entry: the ELF entry point address.
+ *@ptr: A pointer to ELF class array.
+ *Return: Nothing
+ */
+void entry(unsigned long int entry, unsigned char *ptr)
+{
+	printf("  Entry point address:               ");
+	if (ptr[EI_DATA] == ELFDATA2MSB)
+	{
+		entry = ((entry << 8) & 0xFF00FF00) |
+			((entry >> 8) & 0xFF00FF);
+		entry = (entry << 16) | (entry >> 16);
+	}
+	if (ptr[EI_CLASS] == ELFCLASS32)
+		printf("%#x\n", (unsigned int)entry);
+
+	else
+		printf("%#lx\n", entry);
+}
+/**
+ *version - gets the version of an ELF header.
+ *@ptr: A pointer to ELF version array.
+ *Return: Nothing
+ */
+void version(unsigned char *ptr)
+{
+	printf("  Version:                           %d",
+			ptr[EI_VERSION]);
+	if (ptr[EI_VERSION] == EV_CURRENT)
+		printf(" (current)\n");
+	else
+		printf("\n");
+}
 /**
  *type - gets the type of an ELF header.
  *@type: The type of ELF.
@@ -163,29 +183,6 @@ void type(unsigned int type, unsigned char *ptr)
 	else
 		printf("<unknown: %x>\n", type);
 }
-
-/**
- *entry - Prints the entry point of an ELF header.
- *@entry: the ELF entry point address.
- *@ptr: A pointer to ELF class array.
- *Return: Nothing
- */
-void entry(unsigned long int entry, unsigned char *ptr)
-{
-	printf("  Entry point address:               ");
-	if (ptr[EI_DATA] == ELFDATA2MSB)
-	{
-		entry = ((entry << 8) & 0xFF00FF00) |
-			((entry >> 8) & 0xFF00FF);
-		entry = (entry << 16) | (entry >> 16);
-	}
-	if (ptr[EI_CLASS] == ELFCLASS32)
-		printf("%#x\n", (unsigned int)entry);
-
-	else
-		printf("%#lx\n", entry);
-}
-
 /**
  *elf_close - Closes an ELF file.
  *@fd: The file descriptor of the ELF file.
@@ -201,7 +198,7 @@ void elf_close(int fd)
 }
 
 /**
- *main - Displays the information contained in the
+ *main - Entry point
  *Description: displays the information contained in the ELF header
  *@argc: arguments number
  *@argv: array of pointers to the arguments strings
