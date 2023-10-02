20e7a8751a46217a2b5308f320882f743909c778
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-10-02 03:11:39 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,10 +12,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
-	if (fd == -1)
+	if (fd < 0)
 		return (-1);
 	if (text_content == NULL)
-		i = 0;
+	{
+		close(fd);
+		return (1);
+	}
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
 	w = write(fd, text_content, i);
