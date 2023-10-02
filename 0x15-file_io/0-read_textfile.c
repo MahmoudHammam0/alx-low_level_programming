2b5f4cbe6e6ada55324803feb034659430b8ce7d
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-10-02 00:32:23 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the standard output.
+ * @filename: name of file to read from
+ * @letters: number of letters to read from the file
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, r, w, b = letters;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
+	r = read(fd, buff, letters);
+	if (r < 0)
+		return (0);
+	w = write(STDERR_FILENO, buff, letters);
+	if (w < 0 || w < b)
+		return (0);
+	free(buff);
+	close(fd);
+	return (r);
+}
