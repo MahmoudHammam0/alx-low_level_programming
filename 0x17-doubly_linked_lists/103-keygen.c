923ab6d2ebb716a93c3437ccef7ca60dffec1884
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-09 09:30:55 -0800

    11. crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
 	unsigned int idx, v;
 	size_t size, sum;
-	char *str = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char *s = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char h[7] = "      ";
 
 	if (argc != 2)
@@ -20,24 +20,24 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	size = strlen(argv[1]);
-	h[0] = str[(size ^ 59) & 63];
+	h[0] = s[(size ^ 59) & 63];
 	for (idx = 0, sum = 0; idx < size; idx++)
 		sum += argv[1][idx];
-	h[1] = str[(sum ^ 79) & 63];
+	h[1] = s[(sum ^ 79) & 63];
 	for (idx = 0, v = 1; idx < size; idx++)
 		v *= argv[1][idx];
-	h[2] = str[(v ^ 85) & 63];
+	h[2] = s[(v ^ 85) & 63];
 	for (v = argv[1][0], idx = 0; idx < size; idx++)
 		if ((char)v <= argv[1][idx])
 			v = argv[1][idx];
 	srand(v ^ 14);
-	h[3] = str[rand() & 63];
+	h[3] = s[rand() & 63];
 	for (v = 0, idx = 0; idx < size; idx++)
 		v += argv[1][idx] * argv[1][idx];
-	h[4] = str[(v ^ 239) & 63];
+	h[4] = s[(v ^ 239) & 63];
 	for (v = 0, idx = 0; (char)idx < argv[1][0]; idx++)
 		v = rand();
-	h[5] = str[(v ^ 229) & 63];
+	h[5] = s[(v ^ 229) & 63];
 	printf("%s\n", h);
 	return (0);
 }
