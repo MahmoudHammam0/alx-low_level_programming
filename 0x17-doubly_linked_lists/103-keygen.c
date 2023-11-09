957414635e126cec63c41e73fb8b883b0f06b0b7
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-09 09:33:47 -0800

    11. crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -2,10 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 /**
- * main - generate a key depending on a username for crackme5
- * @argc: number of arguments passed
- * @argv: main arguments
- * Return: 0 on success, 1 on error
+ * main - Entry point
+ * Description: generate a keygen for crackme 5
+ * @argc: number of command line arguments
+ * @argv: array of strings of command line arguments
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
