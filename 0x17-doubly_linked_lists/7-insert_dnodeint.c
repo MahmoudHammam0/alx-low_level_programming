a7315bedeecac3e8155e5f76697fa26a175ed274
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-09 05:25:35 -0800

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		idx--;
 	}
 	if (temp1 == NULL)
-		return (NULL)
+		return (NULL);
 	temp2 = temp1->next;
 	if (temp2 == NULL)
 	{
