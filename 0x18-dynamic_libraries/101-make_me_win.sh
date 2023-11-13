7e72185b6bbf610e151bea70fdb3fc4893f412ed
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-13 07:42:16 -0800

    3.

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc *.o -fpic -shared -o million_lib.so
-export LD_PRELOAD="$PWD/million_lib.so"
+#!/bin/bash
+wget -P .. https://raw.githubusercontent.com/hafidaso/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so"
