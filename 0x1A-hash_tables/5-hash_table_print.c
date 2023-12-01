981f8ea2de957c7479a56656448c28fa3c47eaba
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-01 09:40:35 -0800

    5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int count = 0;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			if (count != 0)
+				printf(", ");
+			printf("'%s': ", ht->array[i]->key);
+			printf("'%s'", ht->array[i]->value);
+			count++;
+			node = ht->array[i]->next;
+			while (node)
+			{
+				printf(", ");
+				printf("'%s': ", node->key);
+				printf("'%s'", node->value);
+				node = node->next;
+			}
+		}
+	}
+	printf("}\n");
+}
