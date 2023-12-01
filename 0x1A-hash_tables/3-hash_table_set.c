b284407b8a9ab7cf4d612ed0ef5bc61bc8d4388a
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-01 06:53:05 -0800

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,18 +23,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		for (i = index; ht->array[i]; i++)
-		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = strdup(value);
-			return (1);
+			if (strcmp(ht->array[i]->key, key) == 0)
+			{
+				free(ht->array[i]->value);
+				ht->array[i]->value = strdup(value);
+				return (1);
+			}
 		}
-		else
-		{
-			node->next = ht->array[index];
-			ht->array[index] = node;
-			return (1);
-		}
+		node->next = ht->array[index];
+		ht->array[index] = node;
+		return (1);
 	}
 	return (0);
 }
