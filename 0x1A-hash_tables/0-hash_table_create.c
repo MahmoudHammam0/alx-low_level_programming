6824017dfeb90a32fb08fd53571036735264c5db
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-01 00:47:12 -0800

    0. >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: size of hash table
+ * Return: pointer to hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ptr;
+
+	ptr = malloc(sizeof(hash_table_t) * size);
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
