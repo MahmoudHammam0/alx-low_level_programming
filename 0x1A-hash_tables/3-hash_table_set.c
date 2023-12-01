679bb94c7d91aeee0f4ed8b7a6c50d1b5e30fbcd
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-01 07:28:40 -0800

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node;
-	unsigned long int index = key_index((unsigned char *)key, ht->size), i;
+	unsigned long int index = key_index((const unsigned char *)key, ht->size), i;
 
 	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
 		return (0);
