a8515f917f024cd3a97d143d9132f8c59ba900c6
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-01 11:07:40 -0800

    6. >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: table to be deleted
+ * Return: Nothing
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i = 0;
