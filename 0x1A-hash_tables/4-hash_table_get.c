ad92d3b590cf09975d9626f32a48d169cf1c132a
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-01 08:25:07 -0800

    4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,9 +11,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *item;
 
-	if (ht == NULL || key == NULL || *key == '\0' || ht->size == 0)
+	if (ht == NULL || key == NULL)
 		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
+	if (index > ht->size)
+		return (NULL);
 	item = ht->array[index];
 	if (item == NULL)
 		return (NULL);
