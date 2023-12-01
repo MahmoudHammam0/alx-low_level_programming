5c17a1f0fba442f046e8fabfa5ab1dee5faef6fd
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-01 00:54:20 -0800

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ptr;
 
+	if (size <= 0)
+		return (NULL)
 	ptr = malloc(sizeof(hash_table_t));
 	if (ptr == NULL)
 		return (NULL);
