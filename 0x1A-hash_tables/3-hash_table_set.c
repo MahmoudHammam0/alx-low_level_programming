defd5b5f95d7743bff9dd44f01c35f7b43bfe5de
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-01 03:40:51 -0800

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,7 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		else
 		{
-			node->next = current_item;
+			node->next = ht->array[index];
 			ht->array[index] = node;
 			return (1);
 		}
