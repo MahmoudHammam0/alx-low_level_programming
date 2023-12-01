c7bfc147eca603d94569728f166b3faf18df66ac
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-01 08:21:27 -0800

    4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,17 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: hash table to be searched
+ * @key: item key
+ * Return: value of the key or NULL
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *item;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	item = ht->array[index];
