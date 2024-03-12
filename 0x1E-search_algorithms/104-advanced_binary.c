b444aa32319e7bb75fdaac91186eddfcd9c37ac3
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-12 00:52:12 -0700

    11. Advanced binary search

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+
+int recursive_binary(int *array, int low, int high, int value)
+{
+	int mid = (low + high) / 2, i;
+
+	if (low > high)
+		return (-1);
+	printf("Searching in array: ");
+	i = low;
+	while (i < high)
+	{
+		printf("%d", array[i]);
+		i++;
+		if (i <= high)
+			printf(", ");
+	}
+	printf("%d\n", array[i]);
+	if (array[mid - 1] != value && array[mid] == value)
+		return (mid);
+	else if (array[mid] >= value)
+		return recursive_binary(array, low, mid - 1, value);
+	else
+		return recursive_binary(array, mid + 1, high, value);
+}
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	int low = 0, high = size - 1, index;
+
+	if (array == NULL)
+		return (-1);
+	index = recursive_binary(array, low, high, value);
+	return (index);
+}
